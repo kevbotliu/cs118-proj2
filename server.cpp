38ae7991164ca4df01341273904c967b90f283d9
Kevin Liu <kevinliu732@gmail.com>
2019-03-13 14:25:04 -0700

    Add stdout msgs

--- server.cpp
+++ server.cpp
@@ -19,6 +19,13 @@ int sock_fd;
 struct sockaddr_in server, client;
 std::string file_dir;
 
+enum PrintSetting {
+	Received,
+	Sent,
+	Dropped
+};
+
+
 struct Connection {
 	uint16_t id;
 	FILE* fd;
@@ -62,9 +69,11 @@ void setup(int port) {
 }
 
 
-// int generate_packet(const Packet& p, const Connection& c) {
-// 	return 1;
-// }
+PacketArgs build_response(const Packet& p, const Connection& c) {
+
+	PacketArgs args;
+	return args;
+}
 
 bool update_connection(Packet& p, Connection& conn) {
 	if (p.syn_flag) {
@@ -114,6 +123,33 @@ bool process_packet(Packet& p) {
 
 }
 
+void print_output(PrintSetting action, const Packet& p) {
+	std::string out;
+	bool dup = false;
+	switch (action) {
+		case Received:
+			out = "RECV";
+			break;
+		case Sent:
+			out = "SEND";
+			// dup = true;
+			break;
+		case Dropped:
+			out = "DROP";
+			break;
+	}
+	out += " " + std::to_string(p.seq_num);
+	out += " " + std::to_string(p.ack_num);
+	out += " " + std::to_string(p.conn_id);
+	if (p.ack_flag) out += " ACK";
+	if (p.syn_flag) out += " SYN";
+	if (p.fin_flag) out += " FIN";
+
+	// if (dup) out += " DUP";
+
+	std::cout << out << "\n";
+}
+
 void handle_transfer() {
 
 	// struct timeval tv;
@@ -126,54 +162,37 @@ void handle_transfer() {
 
 	int num_bytes, bytes_written;
     while (1) {
-    	std::cout << "ENTERED\n";
     	memset(buffer, 0, sizeof(buffer));
 
     	if ((num_bytes = recvfrom(sock_fd, buffer, MAX_PACKET_SIZE, 0, (struct sockaddr*)&client, &addr_len)) > 0) {
-    		Packet p = Packet(buffer, num_bytes);
-    		if (!p.is_valid()) continue;
+    		// Received packet
+    		Packet recv = Packet(buffer, num_bytes);
+    		print_output(Received, recv);
+
+    		// Drop if packet invalid
+    		if (!recv.is_valid()) {
+    			print_output(PrintSetting::Dropped, recv);
+    			continue;
+    		}
 
     		PacketArgs args;
     		args.seq_num = 4321;
-    		args.ack_num = p.seq_num + 1;
+    		args.ack_num = recv.seq_num + 1;
     		args.conn_id = 1;
     		args.ack_flag = true;
     		args.syn_flag = true;
     		args.packet_size = 20;
 
-    		Packet new_p = Packet(args);
 
-    		uint32_t* blah = new_p.to_uint32_string();
-			if ((num_bytes = sendto(sock_fd, blah, new_p.size, 0, (struct sockaddr *)&client, addr_len)) <= 0) {
+
+
+    		Packet resp = Packet(args);
+    		uint32_t* resp_buffer = resp.to_uint32_string();
+			if ((num_bytes = sendto(sock_fd, resp_buffer, resp.size, 0, (struct sockaddr *)&client, addr_len)) <= 0) {
 				send_error("Packet sending error occurred.");
 			}
-			std::cout << num_bytes << "\n";
-
-    		// delete blah;
-
-    		// process_packet(p);
-
-
-   //  		uint8_t* buf = p.to_byte_string();
-   //  		std::cout << reinterpret_cast< char const* >(buf)<< "\n";
-   //  		if (p.syn_flag && !p.ack_flag && !p.fin_flag) {
-
-			// 	if ((num_bytes = sendto(sock_fd, buf, p.size, 0, (struct sockaddr *)&client, addr_len)) <= 0) {
-			// 		send_error("Packet sending error occurred.");
-			// 	}
-
-			// }
-			// free(buf);
-
-
-    		// std::cout << std::to_string(p.size) << "\n";
-    		// std::cout << std::to_string(p.seq_num) << "\n";
-    		// std::cout << std::to_string(p.ack_num) << "\n";
-    		// std::cout << std::to_string(p.conn_id) << "\n";
-    		// std::cout << std::to_string(p.ack_flag) << "\n";
-    		// std::cout << std::to_string(p.syn_flag) << "\n";
-    		// std::cout << std::to_string(p.fin_flag) << "\n";
-    		// std::cout << reinterpret_cast< char const* >(p.payload) << "\n";
+			print_output(PrintSetting::Sent, resp);
+    		delete resp_buffer;
 
 
 			// if ((bytes_written = fwrite(buffer, 1, num_bytes, fd)) < 0) send_error("Write error occurred.");
