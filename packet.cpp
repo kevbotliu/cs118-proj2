ce1d3aaee85c97d1ae4d4f88c84e05dd6521266c
Kevin Liu <kevinliu732@gmail.com>
2019-03-13 23:10:18 -0700

    Update server synack, bugfix packet

--- packet.cpp
+++ packet.cpp
@@ -8,14 +8,16 @@ Packet::Packet(const uint8_t buffer[MAX_PACKET_SIZE], int packet_size) : size(pa
 
 		memcpy(&seq_num, buffer, 4);
 		memcpy(&ack_num, buffer+4, 4);
-		memcpy(&flags, buffer+8, 2);
-		memcpy(&conn_id, buffer+10, 2);
-		memcpy(&payload, buffer+12, payload_size);
-
 		seq_num = ntohl(seq_num);
 		ack_num = ntohl(ack_num);
-		conn_id = ntohs(conn_id);
-		flags = ntohs(flags);
+
+		uint32_t id_flags;
+		memcpy(&id_flags, buffer+8, 4);
+		id_flags = ntohl(id_flags);
+		conn_id = id_flags >> 16;
+		flags = id_flags;
+
+		memcpy(&payload, buffer+12, payload_size);
 	}
 }
 
@@ -33,6 +35,7 @@ Packet::Packet(const PacketArgs& args) : size(args.size) {
 
 void Packet::to_uint32_string(uint8_t (&buf)[MAX_PACKET_SIZE]) const {
 	uint32_t val = htonl(seq_num);
+	// std::cout << seq_num << " " << val << "\n";
 	memcpy(buf, &val, 4);
 
 	val = htonl(ack_num);
--- server.cpp
+++ server.cpp
@@ -10,36 +10,47 @@
 #include <cstring>
 #include <fcntl.h>
 #include <signal.h>
-#include <bitset>
-#include <vector>
+#include <map>
 #include "packet.h"
-
+#include <cassert>
 
 int sock_fd;
-struct sockaddr_in server, client;
 std::string file_dir;
 
-enum PrintSetting {
+enum class PrintSetting {
 	Received,
 	Sent,
 	Dropped
 };
 
+enum class PacketStatus {
+	Reply,
+	Drop,
+	Accept
+};
 
-struct Connection {
-	uint16_t id;
+// typedef struct {
+// 	int size;
+// 	uint32_t seq_num, ack_num;
+// 	uint16_t id, flags;
+// 	uint32_t* payload;
+// } Packet;
+
+typedef struct {
 	FILE* fd;
 	uint32_t client_seq_num;
 	uint32_t server_seq_num;
-};
+	bool needs_ack;
+	bool is_closing;
+} Connection;
 
-std::vector<Connection> connections;
 uint16_t curr_max_id = 0;
 
 void setup(int port);
 void send_error(std::string msg, int exit_code = 1);
 void sig_handler(int signum);
 void handle_transfer();
+void print_output(PrintSetting action, const Packet& p);
 
 int main(int argc, char *argv[]) {
 	if (argc != 3) send_error("Must include only <PORT> and <FILE-DIR> arguments.");
@@ -59,91 +70,99 @@ int main(int argc, char *argv[]) {
 }
 
 void setup(int port) {
-	if ((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) send_error("Failed creating socket.");
+	if ((sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) send_error("Failed creating socket.");
 
+	struct sockaddr_in server;
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
+	// server.sin_addr.s_addr = inet_addr("10.0.0.1");
+	// memset(server.sin_zero, '\0', sizeof(server.sin_zero));
 	server.sin_port = htons(port);
 
 	if (bind(sock_fd, (struct sockaddr*)&server, sizeof(server)) < 0) send_error("Failed binding socket to port.");
 }
 
+// bool build_response(const Packet& p, uint16_t conn_id, PacketStatus action, PacketArgs& args) {
+// 	Connection c = connections[conn_id];
 
-PacketArgs build_response(const Packet& p, const Connection& c) {
+// 	args.seq_num = c.server_seq_num;
+// 	args.ack_num = c.client_seq_num;
+// 	args.conn_id = conn_id;
+// 	args.ack_flag = true;
+// 	args.packet_size = HEADER_SIZE;
 
-	PacketArgs args;
-	return args;
-}
+// 	if (p.syn_flag) args.syn_flag = true;
 
-bool update_connection(Packet& p, Connection& conn) {
-	if (p.syn_flag) {
-		uint16_t new_id = curr_max_id + 1;
+// 	return true;
+// }
 
-		std::string filename = file_dir + "/" + std::to_string(new_id) + ".file";
-		std::cout << filename << "\n";
-		FILE *fd = fopen(filename.c_str(), "w+");
-		if (fd == nullptr) send_error("Failed creating new file.");
-		fclose (fd);
+// PacketStatus update_connection_state(Packet p, uint16_t conn_id) {
+// 	Connection c = connections[conn_id];
+// 	if (p.seq_num == c.client_seq_num) {
+// 		// Received FIN packet
+// 		if (p.fin_flag) {
+// 			return PacketStatus::Drop;
+// 		}
+// 		// Received handshake ACK
+// 		else if (p.ack_flag && c.needs_ack) {
+// 			if (p.ack_num == c.server_seq_num + 1) {
+// 				c.needs_ack = false;
+// 				return PacketStatus::Accept;
+// 			}
+// 		}
+// 		// Received general ACK, w/ or w/o payload
+// 		else if (!p.ack_flag) {
+// 			if (p.size == HEADER_SIZE) {
+// 				c.client_seq_num++;
+// 			}
+// 			else c.client_seq_num += p.size - HEADER_SIZE;
+// 			return PacketStatus::Reply;
+// 		}
+// 	}
+// 	return PacketStatus::Drop;
+// }
 
-		conn = {
-			id: new_id,
-			fd: fd,
-			client_seq_num: p.seq_num,
-			server_seq_num: 4321
-		};
-		connections.push_back(conn);
+// uint16_t add_connection_state(Packet p) {
+// 	uint16_t new_id = curr_max_id + 1;
 
-		return true;
-	}
-	else {
-		for (Connection c : connections) {
-			if (p.conn_id == c.id) {
-				conn = c;
-				// Check correct seq and ack numbers
-				if (p.ack_num != c.server_seq_num+1 ||
-					p.seq_num != c.client_seq_num)
-					return false;
+// 	std::string filename = file_dir + "/" + std::to_string(new_id) + ".file";
+// 	std::cout << filename << "\n";
+// 	FILE *fd = fopen(filename.c_str(), "w+");
+// 	if (fd == nullptr) send_error("Failed creating new file.");
 
-				// Update connection values
-				int payload_size = p.size - HEADER_SIZE;
-				if (payload_size) c.client_seq_num += payload_size;
-				else c.client_seq_num += 1;
-			}
-		}
-	}
-}
+// 	Connection c = {
+// 		fd: fd,
+// 		client_seq_num: p.seq_num + 1,
+// 		server_seq_num: 4321,
+// 		needs_ack: true
+// 	};
+// 	connections.insert(std::make_pair(new_id, c));
+// 	curr_max_id++;
 
-bool process_packet(Packet& p) {
-	Connection c;
-	if (update_connection(p, c)) {
-		std::cout << "WOAH " << connections.size() << "\n";
-		// generate_packet(c);
-	}
-
-
-}
+// 	return new_id;
+// }
 
 void print_output(PrintSetting action, const Packet& p) {
 	std::string out;
 	bool dup = false;
 	switch (action) {
-		case Received:
+		case PrintSetting::Received:
 			out = "RECV";
 			break;
-		case Sent:
+		case PrintSetting::Sent:
 			out = "SEND";
 			// dup = true;
 			break;
-		case Dropped:
+		case PrintSetting::Dropped:
 			out = "DROP";
 			break;
 	}
 	out += " " + std::to_string(p.seq_num);
 	out += " " + std::to_string(p.ack_num);
 	out += " " + std::to_string(p.conn_id);
-	if (p.ack_flag) out += " ACK";
-	if (p.syn_flag) out += " SYN";
-	if (p.fin_flag) out += " FIN";
+	if (p.flags & ACK) out += " ACK";
+	if (p.flags & SYN) out += " SYN";
+	if (p.flags & FIN) out += " FIN";
 
 	// if (dup) out += " DUP";
 
@@ -152,64 +171,63 @@ void print_output(PrintSetting action, const Packet& p) {
 
 void handle_transfer() {
 
+	struct sockaddr_in client;
+	socklen_t client_len = sizeof(client);
+
 	// struct timeval tv;
 	// tv.tv_sec = 15;
 	// tv.tv_usec = 0;
 	// setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
 
-	socklen_t addr_len;
-	uint32_t buffer[MAX_PACKET_SIZE/4];
+	uint8_t buffer[MAX_PACKET_SIZE];
+	std::map<uint16_t, Connection> connections;
 
-	int num_bytes, bytes_written;
+	int num_bytes;
     while (1) {
     	memset(buffer, 0, sizeof(buffer));
 
-    	if ((num_bytes = recvfrom(sock_fd, buffer, MAX_PACKET_SIZE, 0, (struct sockaddr*)&client, &addr_len)) > 0) {
-    		// Received packet
-    		Packet recv = Packet(buffer, num_bytes);
-    		print_output(Received, recv);
-
-    		// Drop if packet invalid
-    		if (!recv.is_valid()) {
-    			print_output(PrintSetting::Dropped, recv);
-    			continue;
-    		}
-
-    		PacketArgs args;
-    		args.seq_num = 4321;
-    		args.ack_num = recv.seq_num + 1;
-    		args.conn_id = 1;
-    		args.ack_flag = true;
-    		args.syn_flag = true;
-    		args.packet_size = 20;
+    	if ((num_bytes = recvfrom(sock_fd, buffer, MAX_PACKET_SIZE, 0, (struct sockaddr*)&client, &client_len)) <= 0) {
+    		send_error("Packet receiving error occurred.");
+    	}
 
 
+		// Receive packet
+		Packet recv = Packet(buffer, num_bytes);
+		print_output(PrintSetting::Received, recv);
 
+		uint16_t c_id = recv.conn_id;
 
-    		Packet resp = Packet(args);
-    		uint32_t* resp_buffer = resp.to_uint32_string();
-			if ((num_bytes = sendto(sock_fd, resp_buffer, resp.size, 0, (struct sockaddr *)&client, addr_len)) <= 0) {
-				send_error("Packet sending error occurred.");
-			}
-			print_output(PrintSetting::Sent, resp);
-    		delete resp_buffer;
+		Connection c;
+		if (recv.flags & SYN) {
+			c_id = curr_max_id + 1;
 
+			std::string filename = file_dir + "/" + std::to_string(c_id) + ".file";
+			std::cout << filename << "\n";
+			FILE *fd = fopen(filename.c_str(), "w+");
+			if (fd == nullptr) send_error("Failed creating new file.");
 
-			// if ((bytes_written = fwrite(buffer, 1, num_bytes, fd)) < 0) send_error("Write error occurred.");
+			c = (Connection) {.fd = fd, .client_seq_num = recv.seq_num+1, .server_seq_num = 4321, .needs_ack = true};
+
+			connections[c_id] = c;
+			curr_max_id++;
 		}
-	    else if (num_bytes == 0) break;
-	    // else if (errno == EAGAIN) {
-    	// 	fclose(fd);
-    	// 	fd = fopen(filename.c_str(), "w");
-    	// 	if (fwrite("ERROR: File transfer timeout. Connection aborted.", 1, sizeof(char) * 49, fd) < 0) send_error("Write error occurred.");
-    	// 	break;
-    	// }
-	    else send_error("Packet receiving error occurred.");
+
+		PacketArgs args;
+		args.seq_num = c.server_seq_num;
+		args.ack_num = c.client_seq_num;
+		args.conn_id = c_id;
+		args.flags = 6;
+
+		Packet resp = Packet(args);
+
+		uint8_t out_buffer[MAX_PACKET_SIZE];
+		resp.to_uint32_string(out_buffer);
+
+		if ((num_bytes = sendto(sock_fd, out_buffer, resp.size, 0, (struct sockaddr*)&client, client_len)) <= 0) {
+			send_error("Packet sending error occurred.");
+		}
+		print_output(PrintSetting::Sent, resp);
     }
-
-
-
-	// fclose(sock_fd);
 }
 
 void sig_handler(int signum) { exit(0); }
